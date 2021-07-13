3089bb6d088eb9f0923b82d7cf2cb6cec892826e
Gaoziyang <751928542@qq.com>
2021-07-13 17:20:11 +0800

    1.完善文档。 2.修改Demo案例。

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 LightLock是一个轻量级的、基于Redis实现的分布式锁。
 
+使用LightLock需要依赖到Jedis，具体使用可以参考Demo文件。
+
--- src/main/java/org/light/Demo.java
+++ src/main/java/org/light/Demo.java
@@ -1,24 +1,17 @@
 package org.light;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * @author GaoZiYang
  * @since 2021年07月13日 14:47:30
  */
 public class Demo {
     public static void main(String[] args) {
-        RedisLock redisLock = new RedisLock("172.31.1.106");
+        RedisLock redisLock = new RedisLock("172.0.0.1", 6379);
         try {
             redisLock.lock();
-            try {
-                TimeUnit.SECONDS.sleep(10L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            // 业务逻辑
         } finally {
             redisLock.unlock();
         }
-        System.out.println("继续后续工作");
     }
 }
